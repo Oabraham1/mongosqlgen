9a4cea26722ec1f9c5a01e1e528a200965144394
Oabraham1 <abrahamojima2018@gmail.com>
2022-11-06 23:33:13 -0500

    Started converter

--- internal/converter/convert.go
+++ internal/converter/convert.go
@@ -1,1 +1,20 @@
 package converter
+
+type SQLCommand string
+
+const (
+	Select SQLCommand = "SELECT"
+	Insert SQLCommand = "INSERT"
+	Update SQLCommand = "UPDATE"
+	Delete SQLCommand = "DELETE"
+)
+
+type SQLConverter interface {
+	Convert(command SQLCommand, table string, columns []string, values []interface{}) (string, error)
+}
+
+type SQLConverterFunc func(command SQLCommand, table string, columns []string, values []interface{}) (string, error)
+
+func (f SQLConverterFunc) Convert(command SQLCommand, table string, columns []string, values []interface{}) (string, error) {
+	return f(command, table, columns, values)
+}

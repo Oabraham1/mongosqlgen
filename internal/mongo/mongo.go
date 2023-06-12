04cda432a94c3bd51697b9f03b0233efb8c94273
Oabraham1 <abrahamojima2018@gmail.com>
2023-06-12 19:27:35 -0400

    Modified files

--- internal/mongo/mongo.go
+++ internal/mongo/mongo.go
@@ -35,11 +35,11 @@ func GenerateMongoQuery(query Query) string {
 	}
 }
 
-func GenerateMongoFindQuery(query Query) string{
+func GenerateMongoFindQuery(query Query) string {
 	return fmt.Sprintf("db.%s.%s(%s)", query.Database, query.Collections, query.Filter)
 }
 
-func GenerateMongoInsertQuery(query Query) string{
+func GenerateMongoInsertQuery(query Query) string {
 	fieldsAndValues := ""
 	for i, field := range query.Field {
 		switch query.Values[i].(type) {
@@ -48,24 +48,24 @@ func GenerateMongoInsertQuery(query Query) string{
 		default:
 			fieldsAndValues += fmt.Sprintf("%s: %v", field, query.Values[i])
 		}
-		if i != len(query.Field) - 1 {
+		if i != len(query.Field)-1 {
 			fieldsAndValues += ", "
 		}
 	}
 	return fmt.Sprintf("db.%s.%s({%s})", query.Collections, query.Command, fieldsAndValues)
 }
 
-func GenerateMongoUpdateQuery(query Query) string{
+func GenerateMongoUpdateQuery(query Query) string {
 	fieldsAndValues := ""
 	for i, field := range query.Field {
 		fieldsAndValues += fmt.Sprintf("%s: %s", field, query.Values[i])
-		if i != len(query.Field) - 1 {
+		if i != len(query.Field)-1 {
 			fieldsAndValues += ", "
 		}
 	}
 	return fmt.Sprintf("db.%s.%s.update({%s})", query.Collections, query.Command, fieldsAndValues)
 }
 
-func GenerateMongoDeleteQuery(query Query) string{
+func GenerateMongoDeleteQuery(query Query) string {
 	return fmt.Sprintf("db.%s.%s(%s)", query.Collections, query.Command, query.Filter)
 }
